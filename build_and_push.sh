797d2009d1115461f7f135ba6cc49d23cc94f87e
Ajay Bhargava <bhargava.ajay@gmail.com>
2023-12-31 21:35:33 +0000

    bug: ready for local-training.

--- build_and_push.sh
+++ build_and_push.sh
@@ -17,8 +17,7 @@ fi
 
 account=$(aws sts get-caller-identity --query Account --output text)
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
-region=$(aws configure get region)
-region=${region:-us-west-2}
+region=${region:-us-east-2}
 
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
 echo "ECR image fullname: ${fullname}"
@@ -32,11 +31,8 @@ then
 fi
 
 # Get the login command from ECR and execute it directly
-export _DOCKER_REPO="$(aws ecr get-authorization-token --output text  --query 'authorizationData[].proxyEndpoint')"
-aws ecr get-login --no-include-email --region "${region}" | awk '{print $6}' | docker login -u AWS --password-stdin $_DOCKER_REPO
-
-# Get the login command from ECR in order to pull down the SageMaker PyTorch image
-aws ecr get-login --no-include-email --region "${region}" | awk '{print $6}' | docker login -u AWS --password-stdin "763104351884.dkr.ecr.${region}.amazonaws.com"
+export _DOCKER_REPO="$(aws ecr get-authorization-token --output text --region "${region}" --query 'authorizationData[].proxyEndpoint')"
+aws ecr get-login-password --region "${region}" | docker login -u AWS --password-stdin $_DOCKER_REPO
 
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
--- src/mnist_sagemaker_ci_cd/fit.py
+++ src/mnist_sagemaker_ci_cd/fit.py
@@ -1,32 +1,31 @@
 """Module description goes here."""
 import os
 
-import boto3
 import sagemaker
 from sagemaker.estimator import Estimator
 
-session = sagemaker.Session(boto3.Session(region_name="us-east-1"))
+session = sagemaker.LocalSession()
 
 # AWS Variables
 IAM_ROLE = "arn:aws:iam::220582896887:role/programmatic-aws-sagemaker-role-access"
 ACCOUNT_ID = session.boto_session.client("sts").get_caller_identity()["Account"]
-TRAINING_INSTANCE = "ml.m5.large"
+TRAINING_INSTANCE = "local"
 
 # Github Actions Variables
-GITHUB_REF_NAME = os.environ.get("GITHUB_REF_NAME", "/repository/heads/main")
+GITHUB_REF_NAME = os.environ.get("GITHUB_REF_NAME", "development-training")
 GITHUB_SHA = os.environ.get("GITHUB_SHA", "db056f755716e8a38704402b1bed37a0c0136c48")
-GITHUB_REPOSITORY = os.environ.get("GITHUB_REPOSITORY", "ajay-bhargava/mnist-sagemaker-ci-cd")
-GITHUB_ACTOR = os.environ.get("GITHUB_ACTOR", "ajay-bhargava")
-OUTPUT_S3_URI = (
-    f"s3://with-context-sagemaker-examples/fits/bert-topic/{GITHUB_REF_NAME}/{GITHUB_SHA}"
+GITHUB_REPOSITORY = os.environ.get(
+    "GITHUB_REPOSITORY", "https://github.com/ajay-bhargava/mnist-sagemaker-ci-cd.git"
 )
+GITHUB_ACTOR = os.environ.get("GITHUB_ACTOR", "ajay-bhargava")
+OUTPUT_S3_URI = f"s3://with-context-sagemaker/fits/bert-topic/{GITHUB_REF_NAME}/{GITHUB_SHA}"
 GITHUB_PAT = os.environ.get(
     "GITHUB_PAT",
     "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",
 )
 
 # Dataset Variables (Change this to DVC Repro later)
-ESTIMATOR_DATASET_S3_URI = "s3://with-context-sagemaker-examples/datasets/bert-topic/"
+ESTIMATOR_DATASET_S3_URI = "s3://with-context-sagemaker/datasets/bert-topic/"
 
 # Hyperparameters
 hyperparameters: dict[str, str] = {
@@ -35,12 +34,13 @@ hyperparameters: dict[str, str] = {
 
 # Define Estimator
 estimator = Estimator(
-    image_uri=f"{ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/with-context-sagemaker-examples:latest",
+    image_uri=f"{ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/sagemaker-ecr-bert-topic-example:latest",  # 220582896887.dkr.ecr.us-east-1.amazonaws.com/sagemaker-ecr-bert-topic-example:latest
     role=IAM_ROLE,
     instance_count=1,
     instance_type=TRAINING_INSTANCE,
     hyperparameters=hyperparameters,  # type: ignore
     output_path=OUTPUT_S3_URI,
+    entry_point="src/mnist_sagemaker_ci_cd/fit.py",  # "src/mnist_sagemaker_ci_cd/train.py
     sagemaker_session=session,
     git_config={
         "repo": GITHUB_REPOSITORY,
@@ -49,3 +49,5 @@ estimator = Estimator(
         "token": GITHUB_PAT,
     },
 )
+
+estimator.fit(ESTIMATOR_DATASET_S3_URI)
