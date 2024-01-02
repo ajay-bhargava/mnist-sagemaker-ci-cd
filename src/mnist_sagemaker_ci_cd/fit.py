1f3ec4d82364c862e91f425ed8962a8d637c8d8c
Ajay Bhargava <bhargava.ajay@gmail.com>
2024-01-02 02:01:27 +0000

    bugfix: it appears that only requirements.txt is supportd.

--- src/mnist_sagemaker_ci_cd/fit.py
+++ src/mnist_sagemaker_ci_cd/fit.py
@@ -14,7 +14,7 @@ TRAINING_INSTANCE = "ml.g4dn.xlarge"
 
 # Github Actions Variables
 GITHUB_REF_NAME = os.environ.get("GITHUB_REF_NAME", "development-training")
-GITHUB_SHA = os.environ.get("GITHUB_SHA", "1927b1baa305519c96d186903644216fda399ecc")
+GITHUB_SHA = os.environ.get("GITHUB_SHA", "1927c1baa305519c96d186903644216fda399ecc")
 GITHUB_REPOSITORY = os.environ.get(
     "GITHUB_REPOSITORY", "https://github.com/ajay-bhargava/mnist-sagemaker-ci-cd.git"
 )
@@ -46,7 +46,7 @@ estimator = Estimator(
     code_location=OUTPUT_S3_URI,
     sagemaker_session=session,
     source_dir="src/mnist_sagemaker_ci_cd/",
-    dependencies=["fit-requirements.txt"],
+    dependencies=["src/mnist_sagemaker_ci_cd/requirements.txt"],
     git_config={
         "repo": GITHUB_REPOSITORY,
         "branch": GITHUB_REF_NAME,
