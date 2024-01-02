2a2dc0e1fa06c8e587dbaaa67baefa9fde2e59ea
Ajay Bhargava <bhargava.ajay@gmail.com>
2024-01-02 13:43:47 +0000

    enhancement: moving settings to separate file and creating pydantic class

--- src/mnist_sagemaker_ci_cd/fit.py
+++ src/mnist_sagemaker_ci_cd/fit.py
@@ -1,32 +1,17 @@
 """Module description goes here."""
-import os
-
 import boto3
 import sagemaker
 from sagemaker.estimator import Estimator
 
+from mnist_sagemaker_ci_cd.lib.settings import Settings
+
 session = sagemaker.Session(boto3.Session(region_name="us-east-1"))
+settings = Settings()
 
 # AWS Variables
 IAM_ROLE = "arn:aws:iam::220582896887:role/programmatic-aws-sagemaker-role-access"
 ACCOUNT_ID = session.boto_session.client("sts").get_caller_identity()["Account"]
 TRAINING_INSTANCE = "ml.g4dn.xlarge"
-
-# Github Actions Variables
-GITHUB_REF_NAME = os.environ.get("GITHUB_REF_NAME", "development-training")
-GITHUB_SHA = os.environ.get("GITHUB_SHA", "1929c1baa305519c96d186903644216fda399ecc")
-GITHUB_REPOSITORY = os.environ.get(
-    "GITHUB_REPOSITORY", "https://github.com/ajay-bhargava/mnist-sagemaker-ci-cd.git"
-)
-GITHUB_ACTOR = os.environ.get("GITHUB_ACTOR", "ajay-bhargava")
-GITHUB_PAT = os.environ.get(
-    "GITHUB_PAT",
-    "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",
-)
-
-# Dataset Variables (Change this to DVC Repro later)
-ESTIMATOR_DATASET_S3_URI = "s3://with-context-sagemaker/datasets/bert-topic/"
-OUTPUT_S3_URI = f"s3://with-context-sagemaker/fits/bert-topic/{GITHUB_REF_NAME}/"
 
 # Hyperparameters
 hyperparameters: dict[str, str] = {
@@ -41,18 +26,18 @@ estimator = Estimator(
     entry_point="lib/train.py",
     instance_type=TRAINING_INSTANCE,
     hyperparameters=hyperparameters,  # type: ignore
-    base_job_name=OUTPUT_S3_URI,
-    output_path=OUTPUT_S3_URI,
-    code_location=OUTPUT_S3_URI,
+    base_job_name=settings.output_s3_uri,
+    output_path=settings.output_s3_uri,
+    code_location=settings.output_s3_uri,
     sagemaker_session=session,
     source_dir="src/mnist_sagemaker_ci_cd/",
     dependencies=["src/mnist_sagemaker_ci_cd/deps/train/requirements.txt"],
     git_config={
-        "repo": GITHUB_REPOSITORY,
-        "branch": GITHUB_REF_NAME,
-        "username": GITHUB_ACTOR,
-        "token": GITHUB_PAT,
+        "repo": settings.github_repository,
+        "branch": settings.github_ref_name,
+        "username": settings.github_actor,
+        "token": settings.github_pat,
     },
 )
 
-estimator.fit(ESTIMATOR_DATASET_S3_URI, job_name=f"{GITHUB_SHA[:7]}")
+estimator.fit(settings.estimator_dataset_s3_uri, job_name=f"{settings.github_sha[:7]}")
---
+++ src/mnist_sagemaker_ci_cd/lib/settings.py
@@ -0,0 +1,48 @@
+"""This module contains the settings for the SageMaker Script."""
+import os
+
+from pydantic import BaseSettings, validator
+
+
+class Settings(BaseSettings):
+    """A class that represents the settings for the SageMaker Script.
+
+    Attributes:
+                github_ref_name (str): The name of the GitHub reference (branch or tag) being used. Defaults to "development-training".
+                github_sha (str): The SHA of the GitHub commit being used. Defaults to "1930a1baa305519c96d186903644216fda399ecc".
+                github_repository (str): The URL of the GitHub repository. Defaults to "https://github.com/ajay-bhargava/mnist-sagemaker-ci-cd.git".
+                github_actor (str): The username of the GitHub actor (user or bot) triggering the pipeline. Defaults to "ajay-bhargava".
+                github_pat (str): The GitHub Personal Access Token (PAT) used for authentication. Defaults to a sample token.
+                estimator_dataset_s3_uri (str): The S3 URI for the estimator dataset.
+                output_s3_uri (str): The S3 URI for the output of the pipeline.
+
+    Methods:
+    check_s3_uri(cls, v): A validator method that checks if the provided URI is a valid S3 URI.
+
+    Config:
+    env_file (str): The name of the environment file to load configuration from.
+    """
+
+    github_ref_name: str = os.environ.get("GITHUB_REF_NAME", "development-training")
+    github_sha: str = os.environ.get("GITHUB_SHA", "1930a1baa305519c96d186903644216fda399ecc")
+    github_repository: str = os.environ.get(
+        "GITHUB_REPOSITORY", "https://github.com/ajay-bhargava/mnist-sagemaker-ci-cd.git"
+    )
+    github_actor: str = os.environ.get("GITHUB_ACTOR", "ajay-bhargava")
+    github_pat: str = os.environ.get(
+        "GITHUB_PAT",
+        "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",
+    )
+
+    estimator_dataset_s3_uri: str = os.environ.get(
+        "S3_BASE_PATH", "s3://with-context-sagemaker/datasets/bert-topic/"
+    )
+    output_s3_uri: str = f"s3://with-context-sagemaker/fits/bert-topic/{github_ref_name}/"
+
+    @classmethod
+    @validator("estimator_dataset_s3_uri", "output_s3_uri")
+    def check_s3_uri(cls, v):
+        """A validator method that checks if the provided URI is a valid S3 URI."""
+        if "s3://" not in v:
+            raise ValueError("The URI must be a valid S3 URI")
+        return v
